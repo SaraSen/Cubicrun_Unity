aad2f2c6c3833ce60ea9d79d87dcf4ae762777ff
Saroj Senadheera <ooxygen89@gmail.com>
2018-10-21 16:36:30 +0530

    Initial_Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+    public float Dealay = 2f;
+    public GameObject gameComepleteUI;
+
+
+    public void EndGame() {
+
+        if (gameHasEnded == false) {
+
+            gameHasEnded = true;
+            Debug.Log("over");
+            Invoke("Restart",Dealay);
+        }
+
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void levelComplete() {
+        gameComepleteUI.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PlayerCollition.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollition : MonoBehaviour {
+
+    public PlayerMovement movement;
+   
+
+    public void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "obstacle") {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+            
+        }
+    }
+
+}
